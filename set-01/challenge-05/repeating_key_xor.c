b794975983fedc7fdf6aeebba67dde9802e3bc7b
Jon Eyolfson <jon@eyl.io>
2015-09-08 19:02:22 -0400

    Clean-up Challenge 5 in Set 1

--- set-01/challenge-05/repeating_key_xor.c
+++ set-01/challenge-05/repeating_key_xor.c
@@ -8,7 +8,7 @@
 uint8_t convert_value_to_ascii_hex(uint8_t value)
 {
     if (value >= 16) {
-        exit(2);
+        return 0;
     }
     else if (value < 10) {
         return value + '0';
@@ -24,7 +24,6 @@ size_t repeating_key_xor(const char *input,
                        size_t key_size,
                        char *output,
                        size_t output_max_size)
-
 {
     size_t output_used_size = 0;
     size_t key_index = 0;
@@ -38,8 +37,16 @@ size_t repeating_key_xor(const char *input,
         uint8_t first_4_bits = val >> 4;
         uint8_t last_4_bits = val & 0x0F;
 
-        output[output_used_size] = convert_value_to_ascii_hex(first_4_bits);
-        output[output_used_size + 1] = convert_value_to_ascii_hex(last_4_bits);
+        uint8_t ascii_hex = convert_value_to_ascii_hex(first_4_bits);
+        if (ascii_hex == 0) {
+            return 0;
+        }
+        output[output_used_size] = ascii_hex;
+        ascii_hex = convert_value_to_ascii_hex(last_4_bits);
+        if (ascii_hex == 0) {
+            return 0;
+        }
+        output[output_used_size + 1] = ascii_hex;
         output_used_size += 2;
 
         ++key_index;
@@ -53,14 +60,19 @@ size_t repeating_key_xor(const char *input,
 
 int main()
 {
-    const char * input = "Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal";
+    const char * input = "Burning 'em, if you ain't quick and nimble\n"
+                         "I go crazy when I hear a cymbal";
     size_t input_size = strlen(input);
     const char * key = "ICE";
     size_t key_size = strlen(key);
     size_t output_max_size = 148;
     char output[output_max_size];
 
-    size_t output_used_size = repeating_key_xor(input, input_size, key, key_size, output, output_max_size);
+    size_t output_used_size = repeating_key_xor(input,
+                                                input_size,
+                                                key, key_size,
+                                                output,
+                                                output_max_size);
     if (output_used_size != 0) {
         for (size_t i = 0; i < output_used_size; ++i) {
             printf("%c", output[i]);
