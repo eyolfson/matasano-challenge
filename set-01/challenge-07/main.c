0ef25e697a4eb14264b4bda42b3c6f40430acaca
Jon Eyolfson <jon@eyl.io>
2015-09-14 13:17:48 -0400

    Rewrite Challenge 7 in Set 1

---
+++ set-01/challenge-07/main.c
@@ -0,0 +1,53 @@
+#include "matasano/aes_128.h"
+#include "matasano/utils.h"
+
+#include <stdio.h>
+
+int main()
+{
+    int ret;
+
+    struct mmaped_bytes file_bytes;
+    ret = file_content(&file_bytes, "7.txt");
+    if (ret != 0) {
+        return ret;
+    }
+
+    struct malloced_bytes data_bytes;
+    ret = base64_to_bytes(&data_bytes, file_bytes.data, file_bytes.size);
+    if (ret != 0) {
+        fini_mmaped_bytes(&file_bytes);
+        return ret;
+    }
+
+    struct static_bytes key_bytes;
+    str_literal(&key_bytes, "YELLOW SUBMARINE");
+
+    struct malloced_bytes decrypted_bytes;
+    ret = aes_128_ecb_decrypt(&decrypted_bytes,
+                              key_bytes.data, key_bytes.size,
+                              data_bytes.data, data_bytes.size);
+    if (ret != 0) {
+        fini_malloced_bytes(&data_bytes);
+        fini_mmaped_bytes(&file_bytes);
+        return ret;
+    }
+
+    uint8_t last_byte = decrypted_bytes.data[decrypted_bytes.size - 1];
+    size_t size;
+    if (last_byte < 16) {
+        size = decrypted_bytes.size - last_byte;
+    }
+    else {
+        size = decrypted_bytes.size;
+    }
+
+    for (size_t i = 0; i < size; ++i) {
+        printf("%c", decrypted_bytes.data[i]);
+    }
+
+    fini_malloced_bytes(&decrypted_bytes);
+    fini_malloced_bytes(&data_bytes);
+    fini_mmaped_bytes(&file_bytes);
+    return ret;
+}
