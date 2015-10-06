ed65878bee8681c80869506b326bd989e088d8aa
Jon Eyolfson <jon@eyl.io>
2015-10-06 20:19:41 +0200

    [lib] For PKCS7 no padding bytes is invalid, all padding bytes is valid

--- lib/pkcs7.c
+++ lib/pkcs7.c
@@ -5,14 +5,16 @@
 bool is_valid_pkcs7(const uint8_t *data, size_t size)
 {
     uint8_t last_byte = data[size - 1];
-    if (last_byte >= size) {
-        return true;
+    if (last_byte > size || last_byte == 0) {
+        return false;
     }
 
-    for (ssize_t i = size - 1; i > (size - last_byte - 1); --i) {
+    for (size_t i = size - 1;; --i) {
         if (data[i] != last_byte) {
             return false;
         }
+        if (i == (size - last_byte)) {
+            return true;
+        }
     }
-    return true;
 }
